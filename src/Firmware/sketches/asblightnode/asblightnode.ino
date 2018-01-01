9226907df52203699cd83f342e7b0ae073763f79
Bastian Raschke <bastian.raschke@posteo.de>
2018-01-01 22:50:30 +0100

    Working on light node

--- src/Firmware/sketches/asblightnode/asblightnode.ino
+++ src/Firmware/sketches/asblightnode/asblightnode.ino
@@ -7,32 +7,22 @@
 #define PIN_CAN_CS                              10
 #define PIN_CAN_INT                             2
 
-#define PIN_LED_RED                             14
-#define PIN_LED_GREEN                           13
-#define PIN_LED_BLUE                            12
-#define PIN_LED_WHITE                           0
+#define LED_TYPE                                RGB
 
-#define CROSSFADE_ENABLED                       true
-#define CROSSFADE_DELAY                         2
-#define CROSSFADE_STEPCOUNT                     255
+#define PIN_LED_RED                             5
+#define PIN_LED_GREEN                           3
+#define PIN_LED_BLUE                            6
+#define PIN_LED_WHITE                           9
 
 ASB asb0(NODE_CAN_ADDRESS);
 ASB_CAN asbCan0(PIN_CAN_CS, CAN_125KBPS, MCP_8MHz, PIN_CAN_INT);
 
-const uint32_t defaultOnColor = 0xFFFFFF;
-const uint32_t defaultOffColor = 0x000000;
+bool stateOn = true;
 
-int currentRed = 0;
-int currentGreen = 0; 
-int currentBlue = 0;
-int currentWhite = 0;
-
-int oldRed = currentRed;
-int oldGreen = currentGreen;
-int oldBlue = currentBlue;
-int oldWhite = currentWhite;
-
-int oldButtonState = HIGH;
+enum LEDType {
+    RGB,
+    RGBW
+};
 
 void setup()
 {
@@ -42,7 +32,15 @@ void setup()
     sprintf(buffer, "The node '0x%04X' was powered up.", NODE_CAN_ADDRESS);
     Serial.println(buffer);
 
+    setupLEDs();
     setupCanBus();
+}
+
+void setupLEDs()
+{
+    Serial.println("Setup LEDs...");
+
+    // TODO: show last state
 }
 
 void setupCanBus()
@@ -56,168 +54,90 @@ void setupCanBus()
         Serial.println(F("Attaching CAN was successful."));
 
         const byte bootPacketData[1] = {ASB_CMD_BOOT};
-        byte bootPackedStats = asb0.asbSend(ASB_PKGTYPE_BROADCAST, 0xFFFF, sizeof(bootPacketData), bootPacketData);
+        const byte bootPacketStats = asb0.asbSend(ASB_PKGTYPE_BROADCAST, 0xFFFF, sizeof(bootPacketData), bootPacketData);
+
+        if (bootPacketStats != CAN_OK)
+        {
+            Serial.println(F("The boot complete message could not be sent successfully!"));
+        }
+
+        Serial.println(F("Attaching light state changed hook..."));
+
+        const uint8_t hookOnPacketType = ASB_PKGTYPE_MULTICAST;
+        const uint8_t hookOnTarget = NODE_CAN_ADDRESS;
+        const uint8_t hookOnPort = -1;
+        const uint8_t hookOnFirstDataByte = ASB_CMD_1B; // TODO: Change to ASB_CMD_S_RGBW
+
+        if (asb0.hookAttach(hookOnPacketType, hookOnTarget, hookOnPort, hookOnFirstDataByte, onLightChangedPacketReceived) == false)
+        {
+            Serial.println(F("Attaching light state changed was not successful!"));
+        }
     }
     else
     {
         Serial.println(F("Attaching CAN was not successful!"));
     }
-
-    /**
-     * Type: every type (0xFF)
-     * Target: every target (0x0)
-     * Port: every port (-1)
-     * First data byte: ASB_CMD_1B
-    //  */
-    // Serial.println(F("Attaching light switched hook..."));
-
-    // if (asb0.hookAttach(0xFF, 0x0, -1, ASB_CMD_1B, onLightSwitched) == false)
-    // {
-    //     Serial.println(F("Attaching light switched hook was not successful!"));
-    // }
-
-    // Serial.println(F("Attaching light color hook..."));
-
-    // if (asb0.hookAttach(0xFF, 0x0, -1, ASB_CMD_S_RGB, onColorChanged) == false)
-    // {
-    //     Serial.println(F("Attaching light color hook was not successful!"));
-    // }
 }
 
-void setupLEDStrip()
+void onLightChangedPacketReceived(const asbPacket &canPacket)
 {
-    Serial.println("Setup LED strip...");
-    showGivenColor(defaultOnColor);
+    // TODO: Extract bytes from asbPacket
+
+    const uint8_t redValue = constrain(255, 0, 255);
+    const uint8_t greenValue = constrain(255, 0, 255);
+    const uint8_t blueValue = constrain(255, 0, 255);
+    const uint8_t whiteValue = constrain(255, 0, 255);
+
+    const uint8_t brightness = constrain(255, 0, 255);
+    const bool stateOn = true;
+    const bool fadeEffect = false;
+
+    const uint8_t mappedRedValue = map(redValue, 0, 255, 0, brightness);
+    const uint8_t mappedGreenValue = map(greenValue, 0, 255, 0, brightness);
+    const uint8_t mappedBlueValue = map(blueValue, 0, 255, 0, brightness);
+    const uint8_t mappedWhiteValue = map(whiteValue, 0, 255, 0, brightness);
+
+    // TODO: Pass other variables too?
+    showGivenColor(mappedRedValue, mappedGreenValue, mappedBlueValue, mappedWhiteValue);
+
+    publishLightState();
 }
 
-void onLightSwitched( asbPacket &canPacket)
+void publishLightState()
 {
-    // TODO: add check if setup is done
+    // TODO: Send CAN packet to publish current state
+}
 
-    Serial.print(F("Light was switched "));
+void showGivenColor(const uint8_t redValue, const uint8_t greenValue, const uint8_t blueValue, const uint8_t whiteValue)
+{
+    analogWrite(PIN_LED_RED, redValue);
+    analogWrite(PIN_LED_GREEN, greenValue);
+    analogWrite(PIN_LED_BLUE, blueValue);
 
-    if (canPacket.data[1] == 1)
+    if (LED_TYPE == RGBW)
     {
-        Serial.println("on");
-        showGivenColor(defaultOnColor);
+        analogWrite(PIN_LED_WHITE, whiteValue); 
     }
     else
     {
-        Serial.println("off");
-        showGivenColor(defaultOffColor);
+        analogWrite(PIN_LED_WHITE, 0);   
     }
-}
-
-void onBrightnessChanged( asbPacket &canPacket)
-{
-
-}
-
-void onColorChanged( asbPacket &canPacket)
-{
-    // TODO: add check if setup is done
-
-    Serial.print(F("Color was switched "));
-
-    const uint32_t rColor = canPacket.data[1];
-    const uint32_t gColor = canPacket.data[2];
-    const uint32_t bColor = canPacket.data[3];
-}
-
-void showGivenColor(const uint32_t color)
-{
-    if (CROSSFADE_ENABLED)
-    {
-        showGivenColorWithFadeEffect(color);
-    }
-    else
-    {
-        showGivenColorImmediately(color);
-    }
-}
-
-void showGivenColorWithFadeEffect(const uint32_t color)
-{
-    // // Split new color into its color parts
-    //  int newRed = (color >> 16) & 0xFF;
-    //  int newGreen = (color >> 8) & 0xFF;
-    //  int newBlue = (color >> 0) & 0xFF;
-
-    // // Calulate step count between old and new color value for each color part
-    //  int stepCountRed = calculateStepsBetweenColorValues(oldRed, newRed);
-    //  int stepCountGreen = calculateStepsBetweenColorValues(oldGreen, newGreen); 
-    //  int stepCountBlue = calculateStepsBetweenColorValues(oldBlue, newBlue);
-
-    // for (int i = 0; i <= CROSSFADE_STEPCOUNT; i++)
-    // {
-    //     currentRed = calculateSteppedColorValue(stepCountRed, currentRed, i);
-    //     currentGreen = calculateSteppedColorValue(stepCountGreen, currentGreen, i);
-    //     currentBlue = calculateSteppedColorValue(stepCountBlue, currentBlue, i);
-
-    //  int currentColor = (currentRed << 16) | (currentGreen << 8) | (currentBlue << 0);
-    //     showGivenColorImmediately(currentColor);
-
-    //     delay(CROSSFADE_DELAY);
-    // }
-
-    // oldRed = currentRed; 
-    // oldGreen = currentGreen; 
-    // oldBlue = currentBlue;
-}
-
-int calculateStepsBetweenColorValues(const int oldColorValue, const int newColorValue)
-{
-    int colorValueDifference = newColorValue - oldColorValue;
-
-    if (colorValueDifference)
-    {
-        colorValueDifference = CROSSFADE_STEPCOUNT / colorValueDifference;
-    }
-
-    return colorValueDifference;
-}
-
-int calculateSteppedColorValue( int stepCount,  int currentColorValue,  int i)
-{
-    int steppedColorValue = currentColorValue;
-
-    if (stepCount && (i % stepCount) == 0)
-    {
-        if (stepCount > 0)
-        {              
-            steppedColorValue += 1;           
-        } 
-        else if (stepCount < 0)
-        {
-            steppedColorValue -= 1;
-        } 
-    }
-
-    steppedColorValue = constrain(steppedColorValue, 0, 255);
-    return steppedColorValue;
-}
-
-void showGivenColorImmediately(const uint32_t color)
-{
-    const int rawRed = (color >> 16) & 0xFF;
-    const int rawGreen = (color >> 8) & 0xFF;
-    const int rawBlue = (color >> 0) & 0xFF;
-
-    int mappedRed = map(rawRed, 0, 255, 0, 1024);
-    mappedRed = constrain(mappedRed, 0, 1024);
-
-    int mappedGreen = map(rawGreen, 0, 255, 0, 1024);
-    mappedGreen = constrain(mappedGreen, 0, 1024);
-
-    int mappedBlue = map(rawBlue, 0, 255, 0, 1024);
-    mappedBlue = constrain(mappedBlue, 0, 1024);
-
-    analogWrite(PIN_LED_RED, mappedRed);
-    analogWrite(PIN_LED_GREEN, mappedGreen);
-    analogWrite(PIN_LED_BLUE, mappedBlue);
 }
 
 void loop()
 {
-    asb0.loop();
+    //asb0.loop();
+
+    showGivenColor(50, 0, 0, 255);
+    delay(5000);
+
+    showGivenColor(0, 50, 0, 255);
+    delay(5000);
+
+    showGivenColor(0, 0, 50, 255);
+    delay(5000);
+
+    showGivenColor(255, 100, 50, 255);
+    delay(5000);
+
 }
