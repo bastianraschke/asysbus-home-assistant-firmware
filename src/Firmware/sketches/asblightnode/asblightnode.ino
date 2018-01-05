81b2cc3f73dccd3b0c486ccad075eb68db3fe8d4
Bastian Raschke <bastian.raschke@posteo.de>
2018-01-05 18:39:55 +0100

    Working on light node firmware

--- src/Firmware/sketches/asblightnode/asblightnode.ino
+++ src/Firmware/sketches/asblightnode/asblightnode.ino
@@ -58,7 +58,7 @@ void setupCanBus()
         const uint8_t hookOnPacketType = ASB_PKGTYPE_MULTICAST;
         const uint8_t hookOnTarget = ASB_NODE_ID;
         const uint8_t hookOnPort = -1;
-        const uint8_t hookOnFirstDataByte = ASB_CMD_1B; // TODO: Change to ASB_CMD_S_RGBW
+        const uint8_t hookOnFirstDataByte = ASB_CMD_S_LIGHT;
 
         if (asb0.hookAttach(hookOnPacketType, hookOnTarget, hookOnPort, hookOnFirstDataByte, onLightChangedPacketReceived))
         {
@@ -77,49 +77,46 @@ void setupCanBus()
 
 void onLightChangedPacketReceived(const asbPacket &canPacket)
 {
-    // TODO: Extract bytes from asbPacket
+    // Check if packet is like expected
+    if (canPacket.len == 6)
+    {
+        const bool stateOnOff = canPacket.data[0];
+        const uint8_t brightness = constrain(canPacket.data[1], 0, 255);
 
-    /*
-    Serial.print(F("Type: 0x"));
-    Serial.println(canPacket.meta.type, HEX);
-    Serial.print(F("Target: 0x"));
-    Serial.println(canPacket.meta.target, HEX);
-    Serial.print(F("Source: 0x"));
-    Serial.println(canPacket.meta.source, HEX);
-    Serial.print(F("Port: 0x"));
-    Serial.println(canPacket.meta.port, HEX);
-    Serial.print(F("Length: 0x"));
-    Serial.println(canPacket.len, HEX);
+        const uint8_t redValue = constrain(canPacket.data[2], 0, 255);
+        const uint8_t greenValue = constrain(canPacket.data[3], 0, 255);
+        const uint8_t blueValue = constrain(canPacket.data[4], 0, 255);
+        const uint8_t whiteValue = constrain(canPacket.data[5], 0, 255);
 
-    for(byte i=0; i<canPacket.len; i++) {
-        Serial.print(F(" 0x"));
-        Serial.print(canPacket.data[i], HEX);
+        const uint8_t mappedRedValue = map(redValue, 0, 255, 0, brightness);
+        const uint8_t mappedGreenValue = map(greenValue, 0, 255, 0, brightness);
+        const uint8_t mappedBlueValue = map(blueValue, 0, 255, 0, brightness);
+        const uint8_t mappedWhiteValue = map(whiteValue, 0, 255, 0, brightness);
+
+        // TODO: Pass other variables too?
+        showGivenColor(mappedRedValue, mappedGreenValue, mappedBlueValue, mappedWhiteValue);
+
+        Serial.print(F("onLightChangedPacketReceived(): The light was changed to "));
+
+        Serial.print(F("onLightChangedPacketReceived(): stateOnOff = "));
+        Serial.println(stateOnOff);
+
+        Serial.print(F("onLightChangedPacketReceived(): brightness = "));
+        Serial.println(brightness);
+
+        Serial.print(F("onLightChangedPacketReceived(): redValue = "));
+        Serial.println(redValue);
+        Serial.print(F("onLightChangedPacketReceived(): greenValue = "));
+        Serial.println(greenValue);
+        Serial.print(F("onLightChangedPacketReceived(): blueValue = "));
+        Serial.println(blueValue);
+        Serial.print(F("onLightChangedPacketReceived(): whiteValue = "));
+        Serial.println(whiteValue);
     }
-    */
-
-    const uint8_t redValue = constrain(255, 0, 255);
-    const uint8_t greenValue = constrain(255, 0, 255);
-    const uint8_t blueValue = constrain(255, 0, 255);
-    const uint8_t whiteValue = constrain(255, 0, 255);
-
-    const uint8_t brightness = constrain(255, 0, 255);
-    const bool stateOn = true;
-    const bool fadeEffect = false;
-
-    const uint8_t mappedRedValue = map(redValue, 0, 255, 0, brightness);
-    const uint8_t mappedGreenValue = map(greenValue, 0, 255, 0, brightness);
-    const uint8_t mappedBlueValue = map(blueValue, 0, 255, 0, brightness);
-    const uint8_t mappedWhiteValue = map(whiteValue, 0, 255, 0, brightness);
-
-    // TODO: Pass other variables too?
-    showGivenColor(mappedRedValue, mappedGreenValue, mappedBlueValue, mappedWhiteValue);
-
-    publishLightState();
-}
-
-void publishLightState()
-{
-    // TODO: Send CAN packet to publish current state to hub/brigde
+    else
+    {
+        Serial.println(F("onLightChangedPacketReceived(): Invalid packet!"));
+    }
 }
 
 void showGivenColor(const uint8_t redValue, const uint8_t greenValue, const uint8_t blueValue, const uint8_t whiteValue)
@@ -163,9 +160,5 @@ void loop()
         }
 
         Serial.println();
-    }
-
-    if (canPacket.meta.busId != -1 && canPacket.data[0] != 0x21) {
-        showGivenColor(255, 0, 0, 0);
     }
 }
